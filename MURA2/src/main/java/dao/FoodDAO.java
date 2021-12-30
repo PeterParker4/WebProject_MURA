71ae44dd6373253d91cc94c7bd564b23ac0ac6ca
myjpit <mybsns00@gmail.com>
2021-12-30 11:05:49 +0900

    팀프

--- MURA2/src/main/java/dao/FoodDAO.java
+++ MURA2/src/main/java/dao/FoodDAO.java
@@ -1,5 +1,23 @@
 package dao;
 
-public class FoodDAO {
+import java.sql.Connection;
 
+public class FoodDAO 
+{
+	Connection con;
+	private static FoodDAO  foodDAO;
+	
+	public void setConnection(Connection con)
+	{
+		this.con = con;
+	}
+	
+	public static FoodDAO  getInstance()
+	{
+		if(foodDAO == null)
+		{
+			foodDAO = new FoodDAO ();
+		}
+		return foodDAO;
+	}
 }
